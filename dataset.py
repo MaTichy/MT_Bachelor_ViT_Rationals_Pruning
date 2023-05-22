b01a3b70eb922969376a2eb38341f997e7516d9e
MTichy-CoSci <79090729+MTichy-CoSci@users.noreply.github.com>
2023-05-23 01:02:31 +0200

    first commit

---
+++ dataset.py
@@ -0,0 +1,116 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+from torch import nn
+from torchinfo import summary
+
+import matplotlib.pyplot as plt
+
+from helpers import download_data, create_dataloaders
+
+import os
+from os import getcwd
+
+import random
+import numpy as np
+import pathlib as Path
+
+# Set the batch size
+# 32 # this is lower than the ViT paper but it's because we're starting small
+BATCH_SIZE = 64 #64
+seed = 42   #42
+
+# Create image size (from Table 3 in the ViT paper) 
+IMG_SIZE = 256 
+
+def seed_everything(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+
+seed_everything(seed)
+
+# Download tiny imagenet
+image_path = download_data(source="http://cs231n.stanford.edu/tiny-imagenet-200.zip",
+                           destination="tiny_imagenet_mt")
+
+current = getcwd()
+#image_path = os.path.join(current, image_path )
+
+image_path = current / image_path
+
+image_path = image_path / "tiny-imagenet-200"
+
+# Setup directory paths to train and test images
+train_data = image_path / "train"
+test_data = image_path / "test"
+valid_data0 = image_path / "val"
+
+val_img_dir = os.path.join(valid_data0, 'images')
+
+# Open and read val annotations text file
+fp = open(os.path.join(valid_data0, 'val_annotations.txt'), 'r')
+data = fp.readlines()
+
+# Create dictionary to store img filename (word 0) and corresponding
+# label (word 1) for every line in the txt file (as key value pair)
+valid_data = {}
+for line in data:
+    words = line.split('\t')
+    valid_data[words[0]] = words[1]
+fp.close()
+
+for img, folder in valid_data.items():
+    newpath = (os.path.join(val_img_dir, folder))
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    if os.path.exists(os.path.join(val_img_dir, img)):
+        os.rename(os.path.join(val_img_dir, img), os.path.join(newpath, img))
+
+val_dir = val_img_dir
+
+# Create transform pipeline manually
+manual_transforms = transforms.Compose([
+    transforms.Resize((IMG_SIZE, IMG_SIZE)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(256),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                            std=[0.229, 0.224, 0.225])
+])           
+print(f"Manually created transforms: {manual_transforms}")
+
+# Create data loaders
+train_loader, test_loader, valid_loader, class_names = create_dataloaders(
+    train_dir=train_data,
+    test_dir=test_data,
+    val_dir=val_dir,
+    transform=manual_transforms, # use manually created transforms
+    batch_size=BATCH_SIZE
+)
+
+
+print(len(train_loader), len(test_loader), len(valid_loader))
+
+# Get a batch of images
+image_batch, label_batch = next(iter(train_loader))
+
+"""
+# Get a single image from the batch
+image, label = image_batch[0], label_batch[0]
+
+# View the batch shapes
+#print(image.shape, label)
+
+# Plot image with matplotlib
+plt.imshow(image.permute(1, 2, 0)) # rearrange image dimensions to suit matplotlib [color_channels, height, width] -> [height, width, color_channels]
+plt.title(class_names[label])
+plt.show()
+
+"""
---
+++ dataset2.py
@@ -0,0 +1,84 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+from torch import nn
+from torchinfo import summary
+
+import matplotlib.pyplot as plt
+
+from helpers import NUM_WORKERS
+
+import os
+from os import getcwd
+
+import random
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+
+# Set the batch size
+# 32 # this is lower than the ViT paper but it's because we're starting small
+BATCH_SIZE = 64 #64
+seed = 42   #42
+
+# Create image size (from Table 3 in the ViT paper) 
+IMG_SIZE = 32 # 64
+
+def seed_everything(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+
+seed_everything(seed)
+
+train_transform = transforms.Compose([
+    transforms.Resize((32,32)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32),
+    transforms.ToTensor(),
+    #transforms.Normalize(mean=[0.4377, 0.4438, 0.4728], std=[0.1980, 0.2010, 0.1970])
+])
+
+test_transform = transforms.Compose([
+    transforms.Resize(32),
+    transforms.CenterCrop(32),
+    transforms.ToTensor(),
+    #transforms.Normalize(mean=[0.4377, 0.4438, 0.4728], std=[0.1980, 0.2010, 0.1970])
+])
+
+train_val_set = datasets.SVHN(root='../data/SVHN', split='train', download=True, transform=train_transform)
+test_data = datasets.SVHN(root='../data/SVHN', split='test', download=True, transform=test_transform)
+
+# Accessing labels for training/validation set
+train_val_labels = train_val_set.labels
+
+# Accessing labels for the test set
+test_labels = test_data.labels
+
+train_data, valid_data = train_test_split(train_val_set, 
+                                          test_size=0.2,
+                                          stratify=train_val_labels,
+                                          random_state=seed)
+#train_data, valid_data = torch.utils.data.random_split(train_val_set, [54943, 18314])  # 3/4 of train set for training 1/4 for validation
+#classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+
+train_loader = torch.utils.data.DataLoader(train_data, shuffle=True, batch_size=BATCH_SIZE, drop_last=True)
+valid_loader = torch.utils.data.DataLoader(valid_data, shuffle=True, batch_size=BATCH_SIZE, drop_last=True)
+test_loader = torch.utils.data.DataLoader(test_data, shuffle=False, batch_size=BATCH_SIZE, drop_last=True)
+
+
+print(len(train_loader), len(test_loader), len(valid_loader))
+
+# Get a batch of images
+image_batch, label_batch = next(iter(train_loader))
+
+print(label_batch)
